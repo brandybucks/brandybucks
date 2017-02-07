cfc290c9982f028cefd1165cc94fc7439315e591
Jordan Medina <jordanwmedina@gmail.com>
2017-02-07 14:42:54 -0800

    (feat) Add placeholder Settings component

---
+++ client/app/Settings/Settings.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+export default Settings = () => (
+  <div>
+    This will be the settings page!
+    <Link to="/"><button type="text">Back to DashBoard</button></Link>
+  </div>
+);
