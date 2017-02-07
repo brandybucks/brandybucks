3834d9997693ec4b05461ba269511bde5b59d8f5
Jordan Medina <jordanwmedina@gmail.com>
2017-02-07 14:45:54 -0800

    (feat) Edit Routes to add Dashboard
    -The homepage will now render the Dashboard component instead

--- client/app/routes.jsx
+++ client/app/routes.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './App.jsx';
-import {StudentList} from './StudentList.jsx';
 import CreateLog from './CreateLog.jsx';
 import CreateNote from './CreateNote.jsx';
 import CreateGoal from './CreateGoal.jsx';
@@ -11,11 +10,18 @@ import {IEP} from './IEP.jsx';
 import {MeetingNotes} from './MeetingNotes.jsx';
 import {ViewLogs} from './ViewLogs.jsx';
 import {StudentForm} from './StudentForm.jsx';
-import Charts from './Charts.jsx';
+
+// Dashboard Related Components
+import Dashboard from './Dashboard/Dashboard.jsx';
+import StudentList from './StudentList/StudentList.jsx';
+import Charts from './Statistics/Charts.jsx';
+import Schedule from './Schedule/Schedule.jsx';
+import Settings from './Settings/Settings.jsx';
+
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={StudentList} />
+    <IndexRoute component={Dashboard} />
     <Route path="createlog" component={CreateLog} />
     <Route path="goals" component={Goals} />
     <Route path="iep" component={IEP} />
@@ -24,6 +30,12 @@ export default (
     <Route path="creategoal" component={CreateGoal} />
     <Route path="viewlogs" component={ViewLogs} />
     <Route path="addstudent" component={StudentForm} />
+
+
+
+    <Route path="students" component={StudentList} />
     <Route path="statistics" component={Charts} />
+    <Route path="schedule" component={Schedule} />
+    <Route path="settings" component={Settings} />
   </Route>
 )
