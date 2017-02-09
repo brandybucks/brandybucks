615b3cb8469cbda51964842b80943a73268f0b5b
Jordan Medina <jordanwmedina@gmail.com>
2017-02-09 13:56:34 -0800

    (refactor) Reusable for parents & teachers
    -Dashboard & Panel are shared by parent * teacher now

--- client/app/Dashboard/Dashboard.jsx
+++ client/app/Dashboard/Dashboard.jsx
@@ -24,24 +24,37 @@ class Dashboard extends React.Component {
 
   selectDesc(panel) {
     const descriptions = {
-      welcome: 'Welcome to the llama Teacher Dashboard',
+      welcome: `Welcome to the llama Dashboard`,
+      children: 'Check on your child\'s progress',
       students: 'Check your students\' progress',
-      statistics: 'Visualize classroom metrics with ease',
-      schedule: 'Book appointments with parents as needed',
-      settings: 'Update your personal and professional settings'
+      calendar: 'Manage your appointments with parents',
+      schedule: 'Book appointments with teachers',
+      settings: 'Update your personal settings'
     }
     return descriptions[panel];
   }
 
   render() {
+    let panels = null;
+    if (this.props.status === 'teacher') {
+      panels = [
+        <Panel key={1} hover={this.handleHover} title="Students" icon="graduation-cap" />,
+        <Panel key={2} hover={this.handleHover} title="Calendar" icon="calendar-check-o"/>,
+        <Panel key={3} hover={this.handleHover} title="Settings" icon="cog"/>
+      ]
+    } else {
+      panels = [
+        <Panel key={1} hover={this.handleHover} title="Children" icon="child" />,
+        <Panel key={2} hover={this.handleHover} title="Schedule" icon="calendar-check-o"/>,
+        <Panel key={3} hover={this.handleHover} title="Settings" icon="cog"/>
+      ]
+    }
+
     return (
       <div className="container-fluid">
         <div className="row">
           <div className="col-md-3">
-            <Panel hover={this.handleHover} title="Students" icon="graduation-cap" />
-            <Panel hover={this.handleHover} title="Statistics" icon="bar-chart" />
-            <Panel hover={this.handleHover} title="Schedule" icon="calendar-check-o"/>
-            <Panel hover={this.handleHover} title="Settings" icon="cog"/>
+            {panels}
           </div>
           <div className="col-md-9">
             <Preview img={this.state.preview} desc={this.selectDesc(this.state.panel)}/>
--- client/app/Dashboard/Panel.jsx
+++ client/app/Dashboard/Panel.jsx
@@ -4,7 +4,7 @@ import css from '../../css/panel.css';
 
 const Panel = props => (
   <div className="row">
-    <div className="panel-icon top-15">
+    <div className="panel-icon top-28">
       <Link to={`/${props.title.toLowerCase()}`}>
         <i onMouseOver={() => { props.hover(props.title.toLowerCase()); }} className={`fa fa-${props.icon} fa-5x`}></i>
         <h1>{props.title}</h1>
