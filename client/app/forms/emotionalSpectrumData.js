bfad76f84d14e6fbdc2a21a4062053c45c140a48
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-09 17:24:06 -0800

    (feat) Create dataset of emotions with emoji hex surrogate pairs and scores

---
+++ client/app/forms/emotionalSpectrumData.js
@@ -0,0 +1,57 @@
+let emotionalSpectrum = 
+  [{affect: 'So Fucking Happy', 
+    score: 4,
+    hexCode: {surr1: '0xD83D', 
+              surr2: '0xDE04'
+             }
+    },
+    {affect: 'Really Big Smile', 
+    score: 3,
+    hexCode: {surr1: '0xD83D', 
+              surr2: '0xDE00'
+             }
+    },
+    {affect: 'Big Smile', 
+    score: 2,
+    hexCode: {surr1: '0xD83D', 
+              surr2: '0xDE0A'
+             }
+    },
+    {affect: 'Small Smile', 
+    score: 1,
+    hexCode: {surr1: '0xD83D', 
+              surr2: '0xDE42'
+             }
+    },
+    {affect: 'Neutral', 
+    score: 0,
+    hexCode: {surr1: '0xD83D', 
+              surr2: '0xDE10'
+             }
+    },
+    {affect: 'Slant', 
+    score: -1,
+    hexCode: {surr1: '0xD83D', 
+              surr2: '0xDE15'
+             }
+    },
+    {affect: 'Frown', 
+    score: -2,
+    hexCode: {surr1: '0xD83D', 
+              surr2: '0xDE41'
+             }
+    },
+    {affect: 'Tear Drop', 
+    score: -3,
+    hexCode: {surr1: '0xD83D', 
+              surr2: '0xDE22'
+             }
+    },
+    {affect: 'Crying', 
+      score: -4,
+      hexCode: {surr1: '0xD83D', 
+                surr2: '0xDE2D'
+               }
+    }];
+
+export default emotionalSpectrum;
