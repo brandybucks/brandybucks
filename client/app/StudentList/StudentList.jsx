16660b93f605aa767f8386514483a8e77c64fe9b
Jordan Medina <jordanwmedina@gmail.com>
2017-02-10 11:50:20 -0800

    (refactor) Update to stateless component

--- client/app/StudentList/StudentList.jsx
+++ client/app/StudentList/StudentList.jsx
@@ -1,64 +1,32 @@
 import React from 'react';
-import axios from 'axios';
-import {getAllStudents} from '../helper/auth.js'
-import {StudentEntry} from './StudentEntry.jsx';
 import {Link} from 'react-router';
+import StudentEntry from './StudentEntry.jsx';
 
-export default class StudentList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      students: this.props.student_array,
-      //clickedStudent: {}
-    };
-
-    // this.clickedStudent = this.clickedStudent.bind(this);
-  }
-
-  //method to retrieve student from database once page load.
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props !== nextProps) {
-      this.setState({
-        students: nextProps.student_array});
-    }
-  }
-
-  render () {
-    return (
-      <div id="wrapper">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-12">
-                <h1 className="alignleft">View All Students</h1>
-                <h3 className="alignright"><Link to="/addstudent"><img src="add.png" height="25px" />Student</Link></h3>
-                  <table className="table table-hover table-striped" >
-                    <thead>
-                      <tr>
-                        <th className="col-md-4">Photo</th>
-                        <th className="col-md-2">Last Name</th>
-                        <th className="col-md-2">First Name</th>
-                        <th className="col-md-2">Grade</th>
-                        <th className="col-md-2"># Logs</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {
-                        this.props.student_array.map((student, index) => {
-
-                          return (
-                            <StudentEntry clickedStudent={this.props.clickedStudent} eachStudent={student} key={index} />
-                          )
-                        })
-                      }
-                    </tbody>
-                  </table>
-
-            </div>
-          </div>
+const StudentList = props => (
+  <div id="wrapper">
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className="alignleft">View All Students</h1>
+          <h3 className="alignright"><Link to="/addstudent"><img src="add.png" height="25px" />Student</Link></h3>
+          <table className="table table-hover table-striped" >
+            <thead>
+              <tr>
+                <th className="col-md-4">Photo</th>
+                <th className="col-md-2">Last Name</th>
+                <th className="col-md-2">First Name</th>
+                <th className="col-md-2">Grade</th>
+                <th className="col-md-2"># Logs</th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.students.map((s, i) => <StudentEntry handleClickedStudent={props.handleClickedStudent} student={s} key={i} />)}
+            </tbody>
+          </table>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
+
+export default StudentList;
