2c039991ef701c8239270addbc7ba288bedbd089
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-09 17:22:28 -0800

    (feat) Create EmotionalStateRadio component

---
+++ client/app/forms/EmotionalStateRadio.jsx
@@ -0,0 +1,27 @@
+import React, {Component, PropTypes} from 'react';
+import emotionalSpectrum from './emotionalSpectrumData.js'
+import formStyles from './forms.css';
+
+const EmotionalStateRadio = function({ name, value, hexCode, checked, handleOptionChange }) {
+  return (
+    <div className="radio">
+      <label>
+        <input type="radio" 
+               name={name}
+               value={value} 
+               checked={checked} 
+               onChange={handleOptionChange} />
+        {String.fromCharCode(hexCode.surr1, hexCode.surr2)}
+      </label>
+    </div>
+  );
+}
+
+EmotionalStateRadio.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  hexCode: PropTypes.object.isRequired,
+  checked: PropTypes.bool.isRequired,
+}
+
+export default EmotionalStateRadio;
