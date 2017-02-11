ae0910ee42fa578279724d7ddfda5b83ac3692be
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-11 14:01:10 -0800

    (feat) Add remaining four POST helper functions

--- client/app/helper/formHelpers.js
+++ client/app/helper/formHelpers.js
@@ -7,3 +7,35 @@ exports.addEmotionalStateData = (emotionalStateEntry) => {
     data: emotionalStateEntry
   });
 };
+
+exports.addIntersestEngagementData = (interestEngagementEntry) => {
+  return axios({
+    method: 'POST',
+    url: 'api/interestEngagementData',
+    data: interestEngagementEntry
+  });
+};
+
+exports.addInterpersonalSkillsData = (interpersonalSkillsEntry) => {
+  return axios({
+    method: 'POST',
+    url: 'api/interpersonalSkillsData',
+    data: interpersonalSkillsEntry
+  });
+};
+
+exports.addAttendanceData = (attendanceEntry) => {
+  return axios({
+    method: 'POST',
+    url: 'api/attendanceData',
+    data: attendanceEntry
+  });
+};
+
+exports.addMaturityData = (maturityEntry) => {
+  return axios({
+    method: 'POST',
+    url: 'api/maturityData',
+    data: maturityEntry
+  });
+};
