b54a89baaa8d2a364938c602809140e1d1167487
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-11 12:02:56 -0800

    (feat) Complete radio group component

--- client/app/forms/emotionalStateOption.jsx
+++ client/app/forms/emotionalStateOption.jsx
@@ -5,11 +5,11 @@ import css from '../../css/forms.css';
 const EmotionalStateOption = function({ name, value, handleOptionChange, emotionalSpectrum }) {
   return (
     <div className="radio-group">
-      {emotionalSpectrum.map(({affect: affectValue, hexCode}, index) =>
+      {emotionalSpectrum.map((emotion, index) =>
         <EmotionalStateRadio name={name}
-                             value={affectValue}
-                             hexCode={hexCode}
-                             checked={affectValue === value}
+                             value={emotion.score}
+                             hexCode={emotion.hexCode}
+                             checked={emotion.score === value}
                              handleOptionChange={handleOptionChange}
                              key={index}
         >
@@ -21,7 +21,7 @@ const EmotionalStateOption = function({ name, value, handleOptionChange, emotion
 
 EmotionalStateOption.propTypes = {
   name: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
   handleOptionChange: PropTypes.func.isRequired,
   emotionalSpectrum: PropTypes.array.isRequired
 };
