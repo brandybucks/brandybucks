131b3a4b6ff45fd07d5432e666db9bea7718f57e
Jordan Medina <jordanwmedina@gmail.com>
2017-02-11 14:59:26 -0800

    (fix) Add back click handler

--- client/app/StudentList/StudentEntry.jsx
+++ client/app/StudentList/StudentEntry.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import {Link} from 'react-router';
 
+class StudentEntry extends React.Component {
+  constructor(props) {
+    super(props);
 
-const StudentEntry = props => (
-  <tr onClick={() => { props.handleClickedStudent(props); }}>
-    <td><img src=
-      {props.student.pic} width="40%" /></td>
-      <td>{props.student.last_name}</td>
-      <td>{props.student.first_name}</td>
-      <td>{props.student.grade}</td>
-      <td><Link to="/viewlogs">{props.student.logCount || "None"}</Link></td>
-    </tr>
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-);
+  handleClick() {
+    this.props.handleClickedStudent(this.props.student)
+  }
+
+  render() {  
+    return (
+      <tr onClick={this.handleClick} >
+        <td><img src={this.props.student.pic} width="40%" /></td>
+        <td>{this.props.student.last_name}</td>
+        <td>{this.props.student.first_name}</td>
+        <td>{this.props.student.grade}</td>
+        <td><Link to="/viewlogs">{this.props.student.logCount || "None"}</Link></td>
+      </tr>
+    );
+  }
+
+}
 
 export default StudentEntry;
