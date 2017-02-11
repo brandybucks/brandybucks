c4e68288236dcaac7d74376c04731817ca1ea56e
Jordan Medina <jordanwmedina@gmail.com>
2017-02-11 14:50:47 -0800

    (refactor) Update picture to stay as llama

--- client/app/Nav/NavSide.jsx
+++ client/app/Nav/NavSide.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router';
+import css from '../../css/nav.css';
 
 const NavSide = props => (
   <div className="side-nav">
     <ul className="side-nav">
-      <li className="studentInfo">
-        <img alt="Student Picture" src={props.choosePic()} width="150" />
-        <p>{props.chooseName()}</p>
+      <li className="userInfo">
+        <img className="left-12" alt="User Picture" src={'../llama.png'} width="150" />
       </li>
       {props.list.map((item, i) => (
         <li key={i}>
