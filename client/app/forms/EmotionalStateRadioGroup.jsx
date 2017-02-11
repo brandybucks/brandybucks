b02e329288ce568f4104b59a83ea495a64bc769a
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-11 14:52:50 -0800

    (fix) Renamed this component

---
+++ client/app/forms/EmotionalStateRadioGroup.jsx
@@ -0,0 +1,29 @@
+import React, {Component, PropTypes} from 'react';
+import EmotionalStateRadio from './EmotionalStateRadio.jsx';
+import css from '../../css/forms.css';
+
+const EmotionalStateRadioGroup = function({ name, value, handleOptionChange, emotionalSpectrum }) {
+  return (
+    <div className="radio-group">
+      {emotionalSpectrum.map((emotion, index) =>
+        <EmotionalStateRadio name={name}
+                             value={emotion.score}
+                             hexCode={emotion.hexCode}
+                             checked={emotion.score === value}
+                             handleOptionChange={handleOptionChange}
+                             key={index}
+        >
+        </EmotionalStateRadio>
+      )}
+    </div>
+  );  
+}
+
+EmotionalStateRadioGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  handleOptionChange: PropTypes.func.isRequired,
+  emotionalSpectrum: PropTypes.array.isRequired
+};
+
+export default EmotionalStateRadioGroup;
