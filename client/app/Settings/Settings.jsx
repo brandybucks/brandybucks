6f8aebaf0cb7c766fd00e5adc4fd16add2094c5d
Jordan Medina <jordanwmedina@gmail.com>
2017-02-13 00:54:51 -0800

    (feat) Add placeholder Settings Page

--- client/app/Settings/Settings.jsx
+++ client/app/Settings/Settings.jsx
@@ -1,11 +1,93 @@
 import React from 'react';
 import {Link} from 'react-router';
+import css from '../../css/settings.css';
 
-const Settings = () => (
-  <div>
-    This will be the settings page!
-    <Link to="/"><button type="text">Back to Dashboard</button></Link>
-  </div>
-);
+class Settings extends React.Component {
+
+  // ----------------------------------------------
+  // Lifecycle Methods
+  // ----------------------------------------------
+  componentWillUnmount() {
+    this.props.handleSideBarLinks([]);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.sideBarLinks.length === 0) {
+      this.props.handleSideBarLinks(this.selectSideBarLinks(nextProps.status));
+    }
+  }
+
+  // ----------------------------------------------
+  // Helper Methods
+  // ----------------------------------------------
+  selectSideBarLinks(status) {
+    if (status === 'teacher') {
+      return [
+        {
+          name: 'Home',
+          endpoint: '/'
+        },
+        {
+          name: 'Students',
+          endpoint: '/students'
+        },
+        {
+          name: 'Calendar',
+          endpoint: '/calendar'
+        },
+        {
+          name: 'Settings',
+          endpoint: '/settings'
+        },
+        {
+          name: 'Message',
+          endpoint: '/message'
+        }
+      ]
+    } else {
+      return [
+        {
+          name: 'Home',
+          endpoint: '/'
+        },
+        {
+          name: 'Children',
+          endpoint: '/children'
+        },
+        {
+          name: 'Schedule',
+          endpoint: '/schedule'
+        },
+        {
+          name: 'Settings',
+          endpoint: '/settings'
+        },
+        {
+          name: 'Message',
+          endpoint: '/message'
+        },
+        {
+          name: 'Message',
+          endpoint: '/message'
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <i className="fa fa-wrench fa-5x set-center"></i>
+          <h1>Currently under construction!</h1>
+        </div>
+        <div className="btn-center">
+          <Link to="/"><button className="btn btn-primary" type="text">Back to Dashboard</button></Link>
+        </div>
+      </div>
+    );
+  }
+
+}
 
 export default Settings;
