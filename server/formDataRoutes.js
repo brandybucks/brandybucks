10b6a786110053fbcc332531057ed5dbab148224
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-12 21:59:08 -0800

    (feat) Make a new router for form data and add emotionalStateDataGetAll route

---
+++ server/formDataRoutes.js
@@ -0,0 +1,27 @@
+var controller = require('./controllers/dataControllers');
+var router = require('express').Router();
+
+router.get('/emotionalStateDataGetAll', controller.emotionalState.getAll);
+
+// router.get('/emotionalStateData', controller.emotionalState.get);
+
+router.post('/emotionalStateData', controller.emotionalState.post);
+
+// router.get('/interestEngagementData', controller.interestEngagement.get);
+
+// router.post('/interestEngagementData', controller.interestEngagement.post);
+
+// router.get('/interpersonalSkillsData', controller.interpersonalSkills.get);
+
+// router.post('/interpersonalSkillsData', controller.interpersonalSkills.post);
+
+// router.get('/attendanceData', controller.attendance.get);
+
+// router.post('/attendanceData', controller.attendance.post);
+
+// router.get('/maturityData', controller.maturity.get);
+
+// router.post('/maturityData', controller.maturity.post);
+
+module.exports = router;
+
