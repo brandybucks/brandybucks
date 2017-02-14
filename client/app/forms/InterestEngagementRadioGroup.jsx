2d46bd85a40e63feb22d41c0b48c3650961ae881
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-13 20:04:45 -0800

    (feat) Create InterestEngagementRadioGroup component

--- client/app/forms/InterestEngagementRadioGroup.jsx
+++ client/app/forms/InterestEngagementRadioGroup.jsx
@@ -1,1 +1,30 @@
-InterestEngagementRadioGroup.jsx
+import React, {Component, PropTypes} from 'react';
+import InterestEngagementRadio from './InterestEngagementRadio.jsx';
+import emotionalSpectrum from './emotionalSpectrumData.js';
+import css from '../../css/forms.css';
+
+const InterestEngagementRadioGroup = function({ name, value, handleOptionChange }) {
+  return (
+    <div className="row">
+      <div className="radio-group">
+        {emotionalSpectrum.map((ie, index) =>
+          <InterestEngagementRadio name={name}
+                               value={ie.score}
+                               checked={ie.score === value}
+                               handleOptionChange={handleOptionChange}
+                               key={index}
+          >
+          </InterestEngagementRadio>
+        )}
+      </div>
+    </div>
+  );  
+}
+
+InterestEngagementRadioGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  handleOptionChange: PropTypes.func.isRequired,
+};
+
+export default InterestEngagementRadioGroup;
