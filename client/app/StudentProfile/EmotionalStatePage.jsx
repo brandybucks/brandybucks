e1add09266ca3ed39b054946fe4a9c32e387c4e1
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-13 19:52:37 -0800

    (feat) Add bootstrap class to statistics button and remove EmotionalStateEntryList component

--- client/app/StudentProfile/EmotionalStatePage.jsx
+++ client/app/StudentProfile/EmotionalStatePage.jsx
@@ -6,17 +6,20 @@ import EmotionalStateForm from '../forms/EmotionalStateForm.jsx';
 const EmotionalStatePage = props => (
   <div className="container-fluid">
     <div className="row">
-      <div className="col-md-4">
-        <EmotionalStateEntryList student={props.student} />
-      </div>
-      <div className="col-md-8">
+      <div className="col-lg-12">
         <EmotionalStateForm student={props.student} />
       </div>
     </div>
-    <div className="row-md-12">
-      <Link to="/student/emotionalstatestatistics" className="btn">Statistics</Link>
+    <div className="row">
+      <div className="col-lg-12">
+        <Link to="/student/emotionalstatestatistics" className="btn btn-default">Statistics</Link>
+      </div>
     </div>
   </div>
 );
 
 export default EmotionalStatePage;
+
+      // <div className="col-lg-4">
+      //   <EmotionalStateEntryList student={props.student} />
+      // </div>
