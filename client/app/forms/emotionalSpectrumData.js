2218298b9a3526bc64131b82557710a24cac4888
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-13 20:09:37 -0800

    (fix) Update score range in emotionalSpectrumData  From (-4, 4) to (0, 8)

--- client/app/forms/emotionalSpectrumData.js
+++ client/app/forms/emotionalSpectrumData.js
@@ -1,54 +1,54 @@
 let emotionalSpectrum = 
   [{affect: 'So Fucking Happy', 
-    score: 4,
+    score: 8,
     hexCode: {surr1: '0xD83D', 
               surr2: '0xDE04'
              }
     },
     {affect: 'Really Big Smile', 
-    score: 3,
+    score: 7,
     hexCode: {surr1: '0xD83D', 
               surr2: '0xDE00'
              }
     },
     {affect: 'Big Smile', 
-    score: 2,
+    score: 6,
     hexCode: {surr1: '0xD83D', 
               surr2: '0xDE0A'
              }
     },
     {affect: 'Small Smile', 
-    score: 1,
+    score: 5,
     hexCode: {surr1: '0xD83D', 
               surr2: '0xDE42'
              }
     },
     {affect: 'Neutral', 
-    score: 0,
+    score: 4,
     hexCode: {surr1: '0xD83D', 
               surr2: '0xDE10'
              }
     },
     {affect: 'Slant', 
-    score: -1,
+    score: 3,
     hexCode: {surr1: '0xD83D', 
               surr2: '0xDE15'
              }
     },
     {affect: 'Frown', 
-    score: -2,
+    score: 2,
     hexCode: {surr1: '0xD83D', 
               surr2: '0xDE41'
              }
     },
     {affect: 'Tear Drop', 
-    score: -3,
+    score: 1,
     hexCode: {surr1: '0xD83D', 
               surr2: '0xDE22'
              }
     },
     {affect: 'Crying', 
-      score: -4,
+      score: 0,
       hexCode: {surr1: '0xD83D', 
                 surr2: '0xDE2D'
                }
