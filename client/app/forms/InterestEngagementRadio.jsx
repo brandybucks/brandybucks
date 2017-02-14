a0b284ccabe5f1daf4c284376718d7cca9ff7745
anthonypecchillo <anthonypecchillo@gmail.com>
2017-02-13 20:03:50 -0800

    (feat) Create InterestEngagementRadio component

--- client/app/forms/InterestEngagementRadio.jsx
+++ client/app/forms/InterestEngagementRadio.jsx
@@ -1,1 +1,24 @@
-InterestEngagementRadio.jsx
+import React, {Component, PropTypes} from 'react';
+import css from '../../css/forms.css';
+
+const InterestEngagementRadio = function({ name, value, checked, handleOptionChange }) {
+  return (
+    <div className="radio">
+      <label>
+        <input type="radio" 
+               name={name}
+               value={value} 
+               checked={checked} 
+               onChange={handleOptionChange} />
+      </label>
+    </div>
+  );
+}
+
+InterestEngagementRadio.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  checked: PropTypes.bool.isRequired,
+}
+
+export default InterestEngagementRadio;
